a61073beae1665a7a41546f65bcd3a69529e7e08
Rafael Lisboa <rafael.lisboa0@gmail.com>
2020-11-23 13:23:54 -0300

    new_game_fork

---
+++ JogoCompleto/JogoCompleto/Scripts/Mascarando.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mascarando : MonoBehaviour
+{
+    public int num_mascaras = 3;
+    private Vector2 maskposition;
+    public Rigidbody2D rb;
+
+    public GameObject virus1, virus2, virus3;
+    
+    bool acertei(){
+        Vector2 faceposition = rb.transform.position;
+        if (Vector2.Distance(faceposition, maskposition) < 2f) return true;
+        return false;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        num_mascaras = 3;
+        rb = GameObject.Find("mila").GetComponent<Rigidbody2D> ();
+        maskposition = new Vector2(-0.13f,0.04f);
+        
+        virus1 = GameObject.Find("virus1");
+        virus1.SetActive(false);
+        virus2 = GameObject.Find("virus2");
+        virus2.SetActive(false);
+        virus3 = GameObject.Find("virus3");
+        virus3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            var audios = GetComponents<AudioSource>();
+            
+            
+            if (acertei()) {
+                num_mascaras = 3;
+                audios[1].Play();
+                virus1.SetActive(false);
+                virus2.SetActive(false);
+                virus3.SetActive(false);
+                Debug.Log("Acertou");
+            } else {
+                audios[0].Play();
+                num_mascaras -= 1;
+            }
+        }
+
+        if (num_mascaras <= 2)
+            virus1.SetActive(true);
+
+        if (num_mascaras <= 1)
+            virus2.SetActive(true);
+
+        if (num_mascaras == 0){
+            virus3.SetActive(true);
+            SceneManager.LoadScene("FimdoJogo");
+            Debug.Log("Acabou o jogo");
+        }
+
+    
+    }
+}
---
+++ JogoCompleto/JogoCompleto/Scripts/MovimentoCarinha.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCarinha : MonoBehaviour
+{
+    public float accelerationTime = 4f;
+    public float maxSpeed = 5f;
+    private Vector2 movement;
+    private float timeLeft;
+    public Rigidbody2D rb;
+    private Vector2 maskposition;
+    
+    
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+        StartCoroutine(AdjustVolume());
+        maskposition = new Vector2(-0.13f,0.04f);
+ 
+    }
+    
+    void Update(){
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0){
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timeLeft += accelerationTime;
+        }
+    }
+ 
+    void FixedUpdate(){
+        rb.AddForce(movement * maxSpeed);
+    }
+
+
+    IEnumerator AdjustVolume () {
+     
+     while(true) {
+ 
+         if(GetComponent<AudioSource>().isPlaying) { 
+ 
+            float distanceToTarget = Vector2.Distance(transform.position, maskposition); 
+     
+            if(distanceToTarget < 1) { distanceToTarget = 1; }
+     
+            GetComponent<AudioSource>().volume = 1/distanceToTarget; 
+     
+            yield return new WaitForSeconds(0); 
+     
+            }
+        }
+    }
+}
